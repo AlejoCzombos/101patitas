d0c39f2557059faf29a6d6af14ce2b06893ba038
Alejo <alejoczombos@gmail.com>
2024-03-09 22:09:19 -0300

    ProductCard component

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export function ProductCard({
+  name,
+  description,
+  price,
+}: {
+  name: string;
+  description: string;
+  price: number;
+}) {
+  return (
+    <article
+      key={name}
+      className=" max-w-xs border-2 border-gray rounded-xl overflow-hidden"
+    >
+      <img
+        className="object-cover w-full h-auto hover:scale-105 transition-transform duration-300 ease-in-out"
+        src="https://placehold.it/300x300"
+        alt={name}
+      />
+      <div className="p-4 flex flex-col gap-1">
+        <h3 className="text-2xl font-bold">{name}</h3>
+        <p className="text-3xl">${price}</p>
+        <Link
+          href={`/product?name=${name}`}
+          className="flex justify-center mt-3 border-2 border-primary rounded-full py-2 font-bold text-2xl text-primary hover:bg-primary hover:text-white transition-colors duration-300 ease-in-out"
+        >
+          Comprar
+        </Link>
+      </div>
+    </article>
+  );
+}
