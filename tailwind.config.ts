c90cb2fbc8d638076f2f15471c318298011eed5b
Alejo <alejoczombos@gmail.com>
2024-03-09 22:08:32 -0300

    custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+        primary: '#FF9F00',
+        secondary: '#56CFCB',
+        gray: '#77787C'
+      }
+    }
   },
   plugins: [],
 };
+
 export default config;
