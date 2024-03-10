d1f32589ca17e735a375de45f27df86acac4cf3b
Alejo <alejoczombos@gmail.com>
2024-03-09 22:11:17 -0300

    mapeo de productos y ProductCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,23 @@
-import {api} from '@/api'
-import { Product } from '@/types';
-import Image from 'next/image';
+import { api } from "@/api";
+import Image from "next/image";
+import { ProductCard } from "@/components/ProductCard";
 
 export default async function Home() {
   const products = await api.products.list();
-  
+
   return (
-      <>
-          <h1 className='text-5xl font-bold text-center p-5'>101Patitas</h1>
-          <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
-            {products.map(product => 
-            <article key={product.name} className='max-w-2xl border border-white rounded-lg p-5 flex flex-col items-center'>
-              <img 
-              className='rounded-lg w-40 h-40 object-cover'
-              src='https://placehold.it/150x150'
-              alt='Pelota de futbol'
-              />
-              <h3 className='text-2xl font-bold'>{product.name}</h3>
-              <p>{product.description}</p>
-              <p>${product.price}</p>
-            </article>
-            )}
-          </section>
-          
-      </>
-  )
+    <main className="p-5 mx-auto">
+      <h1 className="text-5xl font-bold text-center p-5">101Patitas</h1>
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+        {products.map((product) => (
+          <ProductCard
+            key={product.name}
+            name={product.name}
+            description={product.description}
+            price={product.price}
+          />
+        ))}
+      </section>
+    </main>
+  );
 }
