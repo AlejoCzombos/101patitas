819b4d447a52f014ea3bbab04674c8e77cca3fce
Alejo <alejoczombos@gmail.com>
2024-03-10 22:01:28 -0300

    estilos desde las props

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,13 +1,25 @@
-export function Carousel({ images, name }: { images: string[]; name: string }) {
+export function Carousel({
+  images,
+  name,
+  className = "",
+}: {
+  images: string[];
+  name: string;
+  className?: string;
+}) {
   return (
     <div
       id="controls-carousel"
-      className="relative w-full bg-red-300"
+      className={`relative w-full bg-red-300 ${className}`}
       data-carousel="static"
     >
-      <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+      <div className="relative aspect-square overflow-hidden rounded-lg">
         {images.map((image) => (
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+          <div
+            key={image}
+            className="hidden duration-700 ease-in-out"
+            data-carousel-item
+          >
             <img
               src={image}
               className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
