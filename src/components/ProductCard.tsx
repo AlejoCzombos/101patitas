38a4e349899c086ad9ef0a38abc3ddcc84ceafa2
Alejo <alejoczombos@gmail.com>
2024-03-12 17:27:35 -0300

    cambios de estilo

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -12,10 +12,10 @@ export function ProductCard({
   return (
     <article
       key={name}
-      className="border-2 w-full border-gray rounded-xl overflow-hidden"
+      className="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300"
     >
       <img
-        className="object-cover w-full h-auto hover:scale-105 transition-transform duration-300 ease-in-out"
+        className="object-cover w-full aspect-square rounded-xl"
         src="https://placehold.it/300x300"
         alt={name}
       />
@@ -23,7 +23,7 @@ export function ProductCard({
         <h3 className="text-2xl font-bold">{name}</h3>
         <p className="text-3xl">${price}</p>
         <Link
-          href={`/product?name=${name}`}
+          href={`/product/${name}`}
           className="flex justify-center mt-3 border-2 border-primary rounded-full py-2 font-bold text-2xl text-primary hover:bg-primary hover:text-white transition-colors duration-300 ease-in-out"
         >
           Comprar
