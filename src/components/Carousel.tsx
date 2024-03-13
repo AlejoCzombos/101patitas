a73b08a97c041ec67ed62d1e0f8f1138dc7de9c5
Alejo <alejoczombos@gmail.com>
2024-03-13 10:18:15 -0300

    corrección del error de la key

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,5 +1,3 @@
-import { useId } from "react";
-
 export function Carousel({
   images,
   name,
@@ -18,7 +16,7 @@ export function Carousel({
       <div className="relative aspect-square overflow-hidden rounded-lg">
         {images.map((image) => (
           <div
-            key={useId()}
+            key={image.length}
             className="hidden duration-700 ease-in-out"
             data-carousel-item
           >
