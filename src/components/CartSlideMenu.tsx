2d69b8a04ffa4fedbb5960991af19475248ca946
Alejo <alejoczombos@gmail.com>
2024-03-14 18:46:38 -0300

    Descripciones de productos en el carrito

--- src/components/CartSlideMenu.tsx
+++ src/components/CartSlideMenu.tsx
@@ -2,10 +2,19 @@
 
 import { useCart, useCartOpen } from "@/store/Cart";
 import type { CartOpenState, CartState } from "@/store/Cart";
+import CloseIcon from "./Icons/CloseIcon";
+import { useState } from "react";
+import { Product } from "@/types";
 
-export default function CartSlideMenu() {
-  const { removeProduct, products } = useCart() as CartState;
+export default function CartSlideMenu({
+  newProducts,
+}: {
+  newProducts: Product[];
+}) {
+  const { removeProduct, cartProducts } = useCart() as CartState;
   const { isOpen, setIsOpen } = useCartOpen() as CartOpenState;
+
+  const [products, setProducts] = useState<Product[]>(newProducts);
 
   const onClose = () => {
     setIsOpen(false);
@@ -24,7 +33,7 @@ export default function CartSlideMenu() {
         className={`
       ${
         isOpen ? "translate-x-0" : "translate-x-full"
-      } h-screen w-[60%] max-w-lg absolute top-0 right-0 z-50 bg-primary-100 transition-transform duration-300 ease-in-out overflow-y-auto border-l-2 border-primary-500`}
+      } h-screen w-[60%] max-w-lg absolute top-0 right-0 z-50 bg-white transition-transform duration-300 ease-in-out overflow-y-auto`}
       >
         <div className="flex justify-between items-center p-4">
           <h2 className="text-2xl font-semibold">Carrito</h2>
@@ -33,81 +42,43 @@ export default function CartSlideMenu() {
             aria-label="Close Cart"
             onClick={onClose}
           >
-            <svg
-              className="size-7"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path d="M6 18L18 6M6 6l12 12"></path>
-            </svg>
+            <CloseIcon className="size-8 hover:transform hover:scale-125 hover:fill-red-500 duration-300" />
           </button>
         </div>
-        {products.map((product) => (
-          <div key={product.name} className="p-4">
-            <div className="flex justify-between items-center">
-              <div className="flex items-center">
+        {cartProducts.map((cartProduct) => (
+          <div key={cartProduct.name} className="p-4">
+            <div className="flex justify-between items-center relative">
+              <div className="flex items-center gap-4">
                 <img
-                  className="h-16 w-16 object-contain"
-                  src="/chapa.png"
+                  className="size-16 object-contain rounded-lg"
+                  src="https://placehold.it/64x64"
                   alt="Chapa"
                 />
-                <div className="ml-4">
-                  <h3 className="text-lg font-semibold">{product.name}</h3>
-                  <p className="text-gray-500">Cantidad: {product.quantity}</p>
+                <div>
+                  <h3 className="text-lg font-semibold">{cartProduct.name}</h3>
+                  <p className="text-gray-500">
+                    Cantidad: {cartProduct.quantity}
+                  </p>
                 </div>
               </div>
+              {products && (
+                <p className="">
+                  ${" "}
+                  {(products.find(
+                    (product) => product.name === cartProduct.name
+                  )?.price ?? 0) * cartProduct.quantity}
+                </p>
+              )}
               <button
-                className="text-2xl"
+                className="absolute top-1 right-1"
                 aria-label="Remove from Cart"
-                onClick={() => removeProduct(product)}
+                onClick={() => removeProduct(cartProduct)}
               >
-                <svg
-                  className="size-7"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M6 18L18 6M6 6l12 12"></path>
-                </svg>
+                <CloseIcon className="size-6 hover:transform hover:scale-125 duration-300" />
               </button>
             </div>
           </div>
         ))}
-        <div className="p-4">
-          <div className="flex justify-between items-center">
-            <div className="flex items-center">
-              <img
-                className="h-16 w-16 object-contain"
-                src="/chapa.png"
-                alt="Chapa"
-              />
-              <div className="ml-4">
-                <h3 className="text-lg font-semibold">Chapa personalizada</h3>
-                <p className="text-gray-500">Cantidad: 1</p>
-              </div>
-            </div>
-            <button className="text-2xl" aria-label="Remove from Cart">
-              <svg
-                className="size-7"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M6 18L18 6M6 6l12 12"></path>
-              </svg>
-            </button>
-          </div>
-        </div>
       </aside>
     </div>
   );
