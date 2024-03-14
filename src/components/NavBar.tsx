a974b3b2a08d772aec99f790b8de66ec5a2b03e3
Alejo <alejoczombos@gmail.com>
2024-03-14 18:47:19 -0300

    peticion de la lista de productos para pasar por parametro al slideMenu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,31 @@
 import Link from "next/link";
-import CartIcon from "./CartIcon";
+import CartButton from "./CartButton";
 import Image from "next/image";
 import CartSlideMenu from "./CartSlideMenu";
+import { api } from "@/api";
 
-export default function NavBar() {
+async function getData() {
+  const products = await api.products.list();
+  return products;
+}
+
+export default async function NavBar() {
+  const products = await getData();
   return (
     <nav className="sticky top-0 z-20 w-full shadow-sm bg-white py-3 sm:py-5 px-6 sm:px-4">
       <div className="container mx-auto flex max-w-6xl flex-wrap items-center justify-between">
         <div className="px-6"></div>
         <Link href="/" className="flex items-center">
           <Image
-            width={500}
-            height={500}
+            width={750}
+            height={300}
             className="size-24 object-contain h-10 w-auto sm:h-12 sm:w-auto"
             src="/logo.png"
             alt="Logo de 101Patitas"
           />
         </Link>
-        <CartIcon />
-        <CartSlideMenu />
+        <CartButton />
+        <CartSlideMenu newProducts={products} />
       </div>
     </nav>
   );
