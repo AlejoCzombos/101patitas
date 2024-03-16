880c00a2bcf820e012099aa9403debe97e137330
Alejo <alejoczombos@gmail.com>
2024-03-16 16:43:51 -0300

    Cambios en los estilos y layout

--- src/components/CartProducts.tsx
+++ src/components/CartProducts.tsx
@@ -8,17 +8,16 @@ import { Product } from "@/types";
 import Link from "next/link";
 
 export default function CartProducts({
-  newProducts,
+  productList,
 }: {
-  newProducts: Product[];
+  productList: Product[];
 }) {
   const { removeProduct, cartProducts, changeQuantity } =
     useCart() as CartState;
   const { isOpen, setIsOpen } = useCartOpen() as CartOpenState;
 
-  const [products, setProducts] = useState<Product[]>(newProducts);
+  const [products, setProducts] = useState<Product[]>(productList);
   const [totalPrice, setTotalPrice] = useState<number>(0);
-  const [quantity, setQuantity] = useState<number>(1);
 
   const onClose = () => {
     setIsOpen(false);
@@ -44,56 +43,63 @@ export default function CartProducts({
   }, [totalPrice, cartProducts]);
 
   return (
-    <section>
-      <div className="flex flex-col justify-between space-y-8">
-        <article className="space-y-5">
-          {cartProducts.map((cartProduct) => (
-            <div
-              key={cartProduct.name}
-              className="grid grid-cols-4 lg:grid-cols-3"
-            >
-              <div className="flex items-center gap-4 col-span-2 lg:col-span-1">
-                <img
-                  className="size-16 object-contain rounded-lg"
-                  src="https://placehold.it/64x64"
-                  alt="Chapa"
-                />
-                <div>
-                  <Link href={`/products/${cartProduct.name}`}>
-                    <h3 className="text-lg font-semibold hover:underline">
-                      {cartProduct.name}
-                    </h3>
-                  </Link>
-                  <p className="text-gray-500">
-                    $ {getPrice(cartProduct.name)}
-                  </p>
-                </div>
+    <section className="flex flex-col justify-between gap-3 bg-white rounded-xl">
+      <header className="space-y-5">
+        <Link className="hover:underline" href={"/"}>
+          {"<"} Continuar comprando
+        </Link>
+        <div className="border-b"></div>
+        <div>
+          <h2 className="text-2xl font-bold">Carrito de compras</h2>
+          <p>Tienes {cartProducts.length} productos en tu carrito</p>
+        </div>
+      </header>
+      <article className="space-y-3">
+        {cartProducts.map((cartProduct) => (
+          <div
+            key={cartProduct.name}
+            className="grid grid-cols-4 lg:grid-cols-3 border p-2 rounded-xl shadow-sm relative gap-4 bg-white hover:shadow-md transition duration-300 ease-in-out"
+          >
+            <div className="flex items-center gap-4 col-span-2 lg:col-span-1">
+              <img
+                className="size-16 object-contain rounded-lg"
+                src="https://placehold.it/64x64"
+                alt="Chapa"
+              />
+              <div>
+                <Link href={`/products/${cartProduct.name}`}>
+                  <h3 className="text-lg font-semibold hover:underline">
+                    {cartProduct.name}
+                  </h3>
+                </Link>
+                <p className="text-gray-500">$ {getPrice(cartProduct.name)}</p>
               </div>
-              <div className="flex justify-center items-center">
-                <select
-                  className="border-none max-w-xs text-center text-xl cursor-pointer"
-                  value={cartProduct.quantity}
-                  onChange={(e) =>
-                    changeQuantity(cartProduct.name, parseInt(e.target.value))
-                  }
-                  name="quantity"
-                  id="quantity"
-                >
-                  {[...Array(10).keys()].map((i) => (
-                    <option key={i} value={i + 1}>
-                      {i + 1}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className="flex justify-end items-center">
-                {products && (
-                  <p className="flex items-end pb-1 text-lg font-bold">
-                    $ {getPrice(cartProduct.name) * cartProduct.quantity}
-                  </p>
-                )}
-              </div>
-              {/*<button
+            </div>
+            <div className="flex justify-center items-center">
+              <select
+                className="border-none max-w-xs text-center text-xl cursor-pointer"
+                value={cartProduct.quantity}
+                onChange={(e) =>
+                  changeQuantity(cartProduct.name, parseInt(e.target.value))
+                }
+                name="quantity"
+                id="quantity"
+              >
+                {[...Array(10).keys()].map((i) => (
+                  <option key={i} value={i + 1}>
+                    {i + 1}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex justify-end items-center">
+              {products && (
+                <p className="flex items-end text-lg font-bold">
+                  $ {getPrice(cartProduct.name) * cartProduct.quantity}
+                </p>
+              )}
+            </div>
+            {/*<button
                 className="absolute top-1 right-1"
                 aria-label="Remove from Cart"
                 onClick={() => {
@@ -103,18 +109,9 @@ export default function CartProducts({
               >
                 <CloseIcon className="size-6 hover:transform hover:scale-125 duration-300" />
             </button>*/}
-            </div>
-          ))}
-        </article>
-        {cartProducts.length > 0 && (
-          <article className="space-y-2">
-            <div className="flex justify-between items-center pb-2 text-3xl font-bold ">
-              <h2>Total:</h2>
-              <h2>$ {totalPrice}</h2>
-            </div>
-          </article>
-        )}
-      </div>
+          </div>
+        ))}
+      </article>
     </section>
   );
 }
