e582b3b1cb2efb7ed3392798888ecfebd4b57e37
Alejo <alejoczombos@gmail.com>
2024-03-21 12:00:41 -0300

    carousel

--- src/app/product/[name]/page.tsx
+++ src/app/product/[name]/page.tsx
@@ -1,6 +1,7 @@
 import { api } from "@/api";
 import QuantitySelector from "@/components/QuantitySelector";
-import { Carousel } from "@components/Carousel";
+//import { Carousel } from "@components/Carousel";
+import { Carousel } from "flowbite-react";
 
 export default async function ProductPage({
   params,
@@ -18,14 +19,23 @@ export default async function ProductPage({
     shape: "Cuadrado",
   };
 
+  console.log(product.images);
+
   return (
     <main className="gap-5 p-5 bg-slate-100">
       <section className="flex flex-col lg:flex-row max-w-5xl gap-5 mx-auto bg-white rounded-xl p-5 lg:p-10">
         <Carousel
-          className="w-full lg:w-1/2"
-          images={product.images || []}
-          name={product.name}
-        />
+          slide={false}
+          className="w-full lg:w-1/2 h-auto aspect-square"
+        >
+          {product.images?.map((image, index) => (
+            <img
+              key={index}
+              src={`/${product.name}/${image}`}
+              alt={product.name}
+            />
+          ))}
+        </Carousel>
         <div className="flex flex-col gap-5 w-full lg:w-1/2">
           <div className="flex flex-col gap-2">
             <p className="text-gray-500 text-sm">
@@ -33,10 +43,10 @@ export default async function ProductPage({
               <span className="text-black font-black"> {product.name}</span>
             </p>
             <h2 className="text-2xl font-bold">{product.name}</h2>
+            <p className="-mt-3">{product.description}</p>
             <p className="text-4xl font-bold">$ {product.price}</p>
           </div>
           <QuantitySelector productName={product.name} />
-          <p>{product.description}</p>
         </div>
       </section>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite-react/lib/**/*.js",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -53,7 +54,8 @@ const config: Config = {
     }
   },
   plugins: [
-    require('@tailwindcss/forms')
+    require('@tailwindcss/forms'),
+    require("flowbite/plugin")
   ],
 };
 
