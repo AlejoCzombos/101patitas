004aba382023b77d106a797dfe590858e7a4b26a
Alejo <alejoczombos@gmail.com>
2024-03-20 22:01:33 -0300

    colores para la seleccion

---
+++ src/data/colors.ts
@@ -0,0 +1,13 @@
+export interface Color {
+    name: string;
+    color: string;
+}
+
+export const colorList: Color[] = [
+    { name: "verde", color: "green" },
+    { name: "azul", color: "blue" },
+    { name: "rojo", color: "red" },
+    { name: "amarillo", color: "yellow" },
+    { name: "rosa", color: "pink" },
+    { name: "naranja", color: "orange" },
+];
